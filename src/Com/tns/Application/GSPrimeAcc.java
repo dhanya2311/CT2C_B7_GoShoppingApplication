ab633a43f9ab9be019681d0707a6d5aa9087b975
dhanya2311 <dhanyahm2397@gmail.com>
2022-02-15 22:44:24 +0530

    Go Shopping Application Completed

---
+++ src/Com/tns/Application/GSPrimeAcc.java
@@ -0,0 +1,31 @@
+
+package Com.tns.Application;
+
+import Com.tns.Framework.PrimeAcc;
+
+public class GSPrimeAcc extends PrimeAcc 
+{
+
+	private static final float charges=0; 
+	
+	
+	public GSPrimeAcc(int accNo, String accNm, float charges, boolean isPrime) 
+	{
+		super(accNo, accNm, charges, isPrime);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void bookProduct(float charges)
+	{
+		
+	}
+
+	@Override
+	public String toString() {
+		return "GSPrimeAcc [isPrime=" + isPrime() + ", accNo=" + accNo + ", charges=" + charges + ", toString()="
+				+ super.toString() + ", getAccNm()=" + getAccNm() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + "]";
+	}
+	
+
+}
---
+++ src/Com/tns/Application/GSShopFactory.java
@@ -0,0 +1,24 @@
+
+package Com.tns.Application;
+
+import Com.tns.Framework.NormalAcc;
+import Com.tns.Framework.PrimeAcc;
+import Com.tns.Framework.ShopFactory;
+
+public class GSShopFactory extends ShopFactory 
+{
+
+	@Override
+	public PrimeAcc getNewPrimeAccount(int AccNo, String accNm, float charges, boolean isPrime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NormalAcc getNewNormalAccount(int AccNo, String accNm, float charges, float deliveryCharges) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
